b24bca9c0425847ea130359b1e50abbef39736f3
Stella Ladegbaye Ewatomi <101569290+LadegbayeStella@users.noreply.github.com>
2022-11-16 22:17:46 +0100

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,62 @@
+// comment can make code readable 
+// welcome to 30days javascript
+let variable = "stella";
+
+// <!-- <!DOCTYPE html>
+// <html lang="en">
+
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="./css/style.css">
+//     <script src="js/jquery-3.6.0.js"></script>
+//     <script src="./js/script.js"></script>
+//     <title>Document</title>
+// </head>
+
+// <body>
+//     <h1 id="hello" class="light">Hello oo Center Of all Languages</h1>
+//      <select>
+//         <option value="black">Black</option>
+//         <option value="red">Red</option>
+//         <option value="blue">Blue</option>
+//         <option value="green">Green</option>
+//     </select> -->
+//     <!-- <br> <br> -->
+//     <!-- <button id="btn">Mode</button> -->
+// <!-- 
+//     <button id="light">Light</button>
+//     <button id="dark">Dark</button>
+
+//     <h1>LETTER TO MR JAVASCRIPT</h1>
+//     <button type="button"
+//         onclick="document.getElementById('output').innerHTML='Mr Javascript, pls Show me the way to Know you😒,Please give me a chance to do the right thing. but You know that we had a deal😂👍😜 I have alot of people to impact😒  '">Click
+//         to read</button>
+//     <p id="output"></p>
+
+//     <h2>What Can JavaScript Do?</h2>
+
+//     <p>JavaScript can change HTML attribute values.</p>
+
+//     <p>In this case JavaScript changes the value of the src (source) attribute of an image.</p>
+
+//     <h6 id="my6">My Lovely javascript</h6>
+//     <button type="button" onclick="document.getElementById('my6').style.fontSize ='190px' ">click</button>
+//     <button type="button" onclick="document.getElementById('image').src='img/pic_bulbon.gif'">click to on the
+//         light</button>
+//     <img id="image" src="./img/pic_bulboff.gif" alt="">
+
+
+//     <button type="button" onclick="document.getElementById('image').src= 'img/pic_bulboff.gif'">click to off the
+//         light</button>
+
+//     <p id="name">myname is stella</p>
+//     <button type="button" onclick="myName()">change</button>
+//  -->
+
+
+// <!-- 
+// </body>
+
+// </html> --> 
